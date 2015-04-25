e6eb6d8d52f5db151d13acf4572fe9f7a7c826af
Jeff Klein <jeff@jeffklein.org>
2015-04-25 00:20:56 -0700

    some very important improvements to when the db schema is refreshed. tested against both the dev and prod maven profiles

--- src/main/resources/org/jeffklein/turfwars/codes/dataaccess/sql/create_users.sql
+++ src/main/resources/org/jeffklein/turfwars/codes/dataaccess/sql/create_users.sql
@@ -2,7 +2,7 @@ SET PASSWORD FOR 'root'@'localhost' = PASSWORD('killMeN0w##');
 SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('killMeN0w##');
 SET PASSWORD FOR 'root'@'::11' = PASSWORD('killMeN0w##');
 
-CREATE DATABASE IF NOT EXISTS twcodes;
+CREATE DATABASE IF NOT EXISTS ${db.name};
 
 CREATE USER 'twcodes'@'localhost' IDENTIFIED BY 'K1lg0r3';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON twcodes.* TO 'twcodes'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON ${db.name}.* TO 'twcodes'@'localhost';
