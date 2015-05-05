891bec7d357107e83d437d9a054781d3427a5ff3
Jeff Klein <jklein@guidewire.com>
2015-05-04 23:55:37 -0700

    Set default timezone for MySQL to be UTC and renamed file to be more generic. not only are we creating users and setting passwords, but we are now setting other metadata such as timezones.

--- src/main/resources/org/jeffklein/turfwars/codes/dataaccess/sql/initDB.sql
+++ src/main/resources/org/jeffklein/turfwars/codes/dataaccess/sql/initDB.sql
@@ -6,3 +6,5 @@ CREATE DATABASE IF NOT EXISTS ${db.name};
 
 CREATE USER 'twcodes'@'localhost' IDENTIFIED BY 'K1lg0r3';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON ${db.name}.* TO 'twcodes'@'localhost';
+
+SET @@global.time_zone='+00:00';
